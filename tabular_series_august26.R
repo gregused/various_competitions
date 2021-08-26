89cc3a74a7b76655ce107dd3a36771ed5d1773a8
gregoryut <grishautkin@gmail.com>
2021-08-26 09:43:39 -0400

    kaggle tabular series august

--- tabular_series_august26.R
+++ tabular_series_august26.R
@@ -33,20 +33,25 @@ xgb_wf <- workflow(rec, xgb_spec)
 folds <- train_raw %>%
   vfold_cv(strata = loss, v = 3)
 
-xgb_tune <- tune_grid(xgb_wf,
-                      folds,
-                      grid = crossing(
-                        trees = seq(900, 1800, 100),
-                        learn_rate = c(0.001, 0.01, 0.02),
-                        mtry = seq(60, 100, 10)
-                      ),
-                      metrics = metric_set(rmse),
-                      control = control_grid(verbose = TRUE))
+xgb_tune <- tune_grid(
+  xgb_wf,
+  folds,
+  grid = crossing(
+    trees = c(1200, 1600, 1800),
+    tree_depth = c(4, 8, 15),
+    learn_rate = c(0.0000749, 0.001, 0.0717),
+    mtry = c(20, 28, 34)
+  ),
+  metrics = metric_set(rmse),
+  control = control_grid(verbose = TRUE)
+)
 
 
 xgb_tune %>%
   collect_metrics() %>%
   arrange(mean)
+
+select_best(xgb_tune)
 
 
 xgb_best <- xgb_wf %>%
@@ -55,8 +60,10 @@ xgb_best <- xgb_wf %>%
 
 
 xgb_best %>%
-  augment(test_raw) %>% select(.pred)
-  mutate(loss = exp(.pred)) %>%
-  select(id, loss) %>% tail()
-  write_csv("sub.csv")
+  augment(test_raw) %>%
+  select(id, loss = .pred) %>%
+  write_csv("sub3.csv")
+
+
+
 
